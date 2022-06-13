dbfd28449b2bc8bb82e32a228671a0466d89ed85
sho032 <murakamishoki0323@yahoo.co.jp>
2022-06-13 16:19:15 +0900

    a

--- laravelapp/resources/views/member/index.blade.php
+++ laravelapp/resources/views/member/index.blade.php
@@ -1,7 +1,13 @@
 @section('content')
     <table>
-    <tr><th>id</th><th>氏名</th><th>生年月日</th><th>年齢</th><th>出身</th></tr>
-    @foreach ($item as $item)
+     <tr>
+        <th>id</th>
+        <th>氏名</th>
+        <th>生年月日</th>
+        <th>年齢</th>
+        <th>出身</th>
+    </tr>
+    @foreach ($items as $item)
         <tr>
             <td>{{$item->ID}}</td>
             <td>{{$item->name}}</td>
