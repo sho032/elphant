ba910c0fbb2063b59bda0a1a9315b39ea6be75b4
sho032 <murakamishoki0323@yahoo.co.jp>
2022-06-14 09:50:37 +0900

    index.blade perfect

--- laravelapp/resources/views/member/index.blade.php
+++ laravelapp/resources/views/member/index.blade.php
@@ -1,4 +1,13 @@
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
     <table>
      <tr>
         <th>id</th>
@@ -14,7 +23,10 @@
             <td>{{$item->birthday}}</td>
             <td>{{$item->age}}</td>
             <td>{{$item->birthplace}}</td>
+            <!-- <td></td>詳細ボタン設置予定 -->
         </tr>
     @endforeach
     </table>
-@endsection
+    
+</body>
+</html>
