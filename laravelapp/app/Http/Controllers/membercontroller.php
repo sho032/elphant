b4cd9a770378b584cb7f64decc09fd2b018b340a
murakami-kousuke <ksw2170338@stu.o-hara.ac.jp>
2022-06-14 10:54:15 +0900

    6/14 更新と削除ページを作成した

--- laravelapp/app/Http/Controllers/membercontroller.php
+++ laravelapp/app/Http/Controllers/membercontroller.php
@@ -12,4 +12,36 @@ class MemberController extends Controller
         $items = DB::select('select * from membertb');
         return view('member.index', ['items' => $items]);
     }
+    public function  edit(Request $request)
+    {
+        $item = DB::table('membertb')
+        ->where('id',$request->id)->first();
+        return view('member.edit', ['form' => $item]);
+    }
+    public function update(Requwst $request)
+    {
+        $param = [
+            'ID' => $request->ID,
+            'name' => $request->name,
+            'birthday' => $request->birthday,
+            'age' => $request->age,
+            'birthplace' => $request-> birthplace,
+        ];
+        DB::table('membertb')
+        ->where('id', $request->id)
+        ->update($param);
+        return redirect('/member');
+    }
+    public function del(Request $request)
+    {
+        $item = DB::table('membertb')
+        ->where('id',$request->id)->first();
+        return view('member.del', ['form' => $item]);
+    }
+    public function remove(Request $request)
+    {
+        DB::table('membertb')
+        ->where('id',$request->id)->delete();
+        return redirect('/member');
+    }
 }
