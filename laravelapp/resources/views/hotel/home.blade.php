353a4c44ea96f685f3541db980ab73c5eaf73bb4
Yosida-Ayuto <ksw2170029@stu.o-hara.ac.jp>
2022-06-21 10:03:13 +0900

    home修正版

--- laravelapp/resources/views/hotel/home.blade.php
+++ laravelapp/resources/views/hotel/home.blade.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ホーム</title>
-    <a href="hotel/login">login</a>
-</head>
-<body>
-    <!-- <p><img src"" alt="hotelの写真"  -->
-    <a href="/hotel/search">部屋の予約状況の確認</a>
-</body>
-
-<foter>
-    <p>☎0123-012-0003</p>
-</foter>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>追加ページ</title>
+        
+    </head>
+    <body>
+        <a href="/hotel/search">部屋の予約状況の確認</a>
+    </body>
 </html>
