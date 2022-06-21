459e27d8c4016d44db22487b6dab600d80af2557
Yosida-Ayuto <ksw2170029@stu.o-hara.ac.jp>
2022-06-21 12:14:01 +0900

    aa

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -55,4 +55,8 @@ Route::get('member/del2','MemberController@del2');
 Route::get('home', 'HotelController@home');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('hotel/search', 'HotelController@find');
+Route::post('hotel/search', 'HotelController@search');
+
+Route::get('/', 'HomeController@index')->name('home');
+
