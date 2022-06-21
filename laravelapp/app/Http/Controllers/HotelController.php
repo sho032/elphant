6145c77e56a27656f259b3f1076b38975a6a6fa3
Yosida-Ayuto <ksw2170029@stu.o-hara.ac.jp>
2022-06-21 11:43:30 +0900

    hotel

--- laravelapp/app/Http/Controllers/HotelController.php
+++ laravelapp/app/Http/Controllers/HotelController.php
@@ -14,4 +14,19 @@ class HotelController extends Controller
         return view('hotel.home');
     }
 
+    public function find(Request $request)
+    {
+
+    return view('hotel.search',['cheak' => '']);
+    }
+
+    public function search(Request $request)
+    {
+        $cheak=$request -> cheak;
+        $item = reserve::find($request->cheak);
+        $param = ['cheak' => $request-> $item];
+
+        return view('hotel.search', $param);
+    }
+
 }
--- laravelapp/resources/views/hotel/search.blade.php
+++ laravelapp/resources/views/hotel/search.blade.php
@@ -4,9 +4,57 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>部屋検索</title>
+    <title>ログイン</title>
+    <a href="hotel/login">メンバーの追加</a>
 </head>
 <body>
 
+    <form action="/hotel/login" method="post">
+        @csrf
+        <p>チェックイン: </th><td><input type="date" name="check" value="{{check}}"></p>
+        <input type="submit" value="検索">
+    </form>
+    @if(isset($item))
+    <table>
+    <tr><th>予約可能</th></tr>
+    <tr>
+        <td>{{$item->getdate()}}</td>
+    </tr>
+    </table>
+    @endif
 </body>
 </html>
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+    <a href="hotel/login">メンバーの追加</a>
+</head>
+<body>
+
+    <form action="/hotel/login" method="post">
+        @csrf
+        <p>チェックイン: </th><td><input type="date" name="check" value="{{check}}"></p>
+        <input type="submit" value="検索">
+    </form>
+    @if(isset($item)){
+        <p>予約不可</p>   
+    }@else{
+        <table>
+            <tr><th>予約可能</th></tr>
+            <form action="/hotel/reserv" method="post">
+                @csrf
+                <tr><td>{{cheak}}</td></tr>
+                <tr><th></th><td><input type="submit" value="予約"></td></tr>
+            </form>
+        </table>
+    }
+    @endif
+</body>
+</html> -->
