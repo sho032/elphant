2750446e300cecd48d18f7fe03d7d4d6fab83af7
sho032 <murakamishoki0323@yahoo.co.jp>
2022-06-21 12:12:53 +0900

    search

--- laravelapp/resources/views/hotel/search.blade.php
+++ laravelapp/resources/views/hotel/search.blade.php
@@ -4,23 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ログイン</title>
-    <a href="hotel/login">メンバーの追加</a>
+    <title>部部屋検索</title>
+    <!-- <a href="hotel/login">メンバーの追加</a> -->
 </head>
 <body>
 
-    <form action="/hotel/login" method="post">
+    <form action="/hotel/search" method="post">
         @csrf
-        <p>チェックイン: </th><td><input type="date" name="check" value="{{check}}"></p>
+        <p>チェックイン日(年月日):<input type="date" name="check" value="{{check}}"></p>
         <input type="submit" value="検索">
     </form>
+    <!-- @if(isset($item)){
+        <table>
+            <tr>
+                <th>予約可能</th>
+            </tr>
+            <tr>
+                <td>{{$item->getdate()}}</td>
+            </tr>
+        </table>
+    }
+    @endif -->
+    <!-- 一致する日付があれば予約不可 -->
     @if(isset($item))
-    <table>
-    <tr><th>予約可能</th></tr>
-    <tr>
-        <td>{{$item->getdate()}}</td>
-    </tr>
-    </table>
+        <p>予約不可</p>   
+    @else
+        <table>
+            <tr><th>予約可能</th></tr>
+            <form action="/hotel/reserv" method="post">
+                @csrf
+                <tr><td>{{cheak}}</td></tr>
+                <tr><th></th><td><input type="submit" value="予約"></td></tr>
+            </form>
+        </table>
+    
     @endif
 </body>
 </html>
